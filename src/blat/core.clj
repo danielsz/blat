f5b64f2a6f2a40d9f7c103085e8d35de2ff126c3
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2020-04-08 14:13:21 +0300

    Formatting.
    - Link to cljdocs

--- src/blat/core.clj
+++ src/blat/core.clj
@@ -7,10 +7,14 @@
   number. The second argument is a channel on which to write the page
   results. f should return immediately. If it does any kind of i/o,
   use an asynchronous library. If it needs to compute, make it asynchronous.
+
   `start` is the first page.
+
   `end` is the last page.
+
   `coll` is the initial collection of results. Defaults to an empty
   vector.
+
   `xf` is a transducer that will be applied when reading the channel
   that was passed to `f`. Defaults to a no-op."
   ([f start end]
